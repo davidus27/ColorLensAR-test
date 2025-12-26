bb64c5e961403f096044f41884d96c6b5e921ccf
davidus27 <davidisbest27@gmail.com>
2025-12-26 12:19:26 +0100

    update ci jobs

---
+++ app/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  extends: [
+    'expo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    // Relax some rules for easier development
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'android/',
+    'ios/',
+    '.expo/',
+    'coverage/',
+    'build/',
+  ],
+};
+
